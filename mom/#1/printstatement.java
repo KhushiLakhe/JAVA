86ea5296fe2e85c094e5189185908ba60c5f5d86
KhushiLakhe <khushilakhe@gmail.com>
2025-08-09 14:34:24 +0530

    Delete mom directory

--- mom/#1/printstatement.java
+++
@@ -1,8 +0,0 @@
-public class printstatement {
-public static void main(String[] args) {
-    System.out.println("*");
-    System.out.println("**");
-    System.out.println("***");
-    System.out.println("****");
-}    
-}
--- mom/#4 loops/q2.java
+++
@@ -1,7 +0,0 @@
-public class q2 {
-    public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
-            System.out.println("apna sapna money money");
-        }
-    }
-}
